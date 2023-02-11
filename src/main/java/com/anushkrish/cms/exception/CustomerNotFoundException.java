cd5c34a4ba5af0e172a036a58673da6c5cdd7343
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2023-02-12 03:21:59 +0530

    CustomerNotffoundException
    exception handled with customise error code and message.

--- src/main/java/com/anushkrish/cms/exception/CustomerNotFoundException.java
+++ src/main/java/com/anushkrish/cms/exception/CustomerNotFoundException.java
@@ -1,9 +1,6 @@
 package com.anushkrish.cms.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class CustomerNotFoundException extends RuntimeException{
 
     public CustomerNotFoundException( String message){
