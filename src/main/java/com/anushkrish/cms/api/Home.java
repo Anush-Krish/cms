9d2cd879dade10bcbb6bbb419af609976215a57a
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2023-02-12 03:48:19 +0530

    FIx WhiteLabel error page
    Fixed white label error page with hone api.

---
+++ src/main/java/com/anushkrish/cms/api/Home.java
@@ -0,0 +1,15 @@
+package com.anushkrish.cms.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class Home {
+
+    @GetMapping
+    public String home(){
+       return "Application is running."+new Date();
+    }
+}
