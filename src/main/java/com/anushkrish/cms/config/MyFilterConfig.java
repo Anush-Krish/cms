bde43d0acf47449417da596c20ec48e2f436e2dd
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2023-02-15 01:47:16 +0530

    annotation bean added

--- src/main/java/com/anushkrish/cms/config/MyFilterConfig.java
+++ src/main/java/com/anushkrish/cms/config/MyFilterConfig.java
@@ -2,10 +2,12 @@ package com.anushkrish.cms.config;
 
 import com.anushkrish.cms.filter.MyNewFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MyFilterConfig {
+    @Bean
     public FilterRegistrationBean<MyNewFilter>  registrationBean(){
         FilterRegistrationBean<MyNewFilter> registrationBean=new FilterRegistrationBean<>();
 
