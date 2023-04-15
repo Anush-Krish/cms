5bd639e47a58030b73731660692ed2c8d2859206
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2023-04-16 01:27:23 +0530

    OAuthConfiguration setup

---
+++ src/main/java/com/anushkrish/cms/security/ServerSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.anushkrish.cms.security;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+}
---
+++ src/main/java/com/anushkrish/cms/security/UserService.java
@@ -0,0 +1,11 @@
+package com.anushkrish.cms.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+
+}
